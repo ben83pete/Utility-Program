7f792984dbe3a6dbc83582edc2316d53570bb11c
ben Petersen <ben83pete@gmail.com>
2019-02-07 11:01:27 -0500

    Utility program

---
+++ BenUtilityLibrary/Strings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenUtilityLibrary
+{
+    public class Strings
+    {
+        public static bool EqualIgnoreCase(string strA, string strB)  // pass 2 strings into method EqualIgnoreCase
+        {
+            var compareResult = string.Compare(strA.ToUpper(), strB.ToUpper()); // compare the 2 strings to see if they are equal, string.Compare will return an integer -1, 0, or 1
+                                            //  .ToUpper will make all char in the string upper case.
+            bool returnResult;
+
+            if( compareResult == 0){
+                returnResult = true;
+            }else{
+                returnResult = false;
+            }
+            return returnResult;   
+
+        }
+    }
+}
---
+++ TestBenUtility/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenUtilityLibrary;
+
+namespace TestBenUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Strings.EqualIgnoreCase("ABC", "abc");
+            if (result) { // [ if (result) ] is the same as [ if (result == true) ]
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("not Equal");
+            }
+            Console.ReadKey();
+        }
+    }
+}
